938402f972faf656440c500fff6a50240f188fcb
chisan01 <chisan2001@gmail.com>
2022-11-04 17:47:48 +0900

    initial project setup

---
+++ src/test/java/com/samdoree/fieldgeolog/FieldGeoLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samdoree.fieldgeolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldGeoLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
