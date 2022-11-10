a8de554602583df217373eb76da13e288bf3ce10
JinYoungLee <brenda524@naver.com>
2022-11-10 15:36:39 +0900

    chore: Repository의 불필요한 쿼리 메소드 삭제

--- src/main/java/com/samdoree/fieldgeolog/Spot/Repository/SpotRepository.java
+++ src/main/java/com/samdoree/fieldgeolog/Spot/Repository/SpotRepository.java
@@ -4,9 +4,7 @@ import com.samdoree.fieldgeolog.Spot.Entity.Spot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.List;
 
 @EnableJpaAuditing
 public interface SpotRepository extends JpaRepository<Spot, Long> {
-    List<Spot> findAll();
 }
