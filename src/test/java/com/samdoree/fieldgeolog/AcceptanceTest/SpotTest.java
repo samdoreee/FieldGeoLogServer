f46da319c16b2cc6eb95858e2373dfddadc7ce45
chisan01 <chisan2001@gmail.com>
2022-12-01 18:12:29 +0900

    docs: SpotTest rest-docs 문서화
    - asciidoc 관련 의존성 추가
    - 문서화 관련 gradle task 추가
    - 테스트 코드에 문서화 관련 설정 추가
    - 생성된 문서 경로 .gitignore에 추가

--- src/test/java/com/samdoree/fieldgeolog/AcceptanceTest/SpotTest.java
+++ src/test/java/com/samdoree/fieldgeolog/AcceptanceTest/SpotTest.java
@@ -6,17 +6,27 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.samdoree.fieldgeolog.ApiDocumentUtils.getDocumentRequest;
+import static com.samdoree.fieldgeolog.ApiDocumentUtils.getDocumentResponse;
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class)
 class SpotTest {
 
     @LocalServerPort
@@ -25,24 +35,39 @@ class SpotTest {
     private RequestSpecification basicRequest;
 
     @BeforeEach
-    void setUp() {
+    void setUp(RestDocumentationContextProvider restDocumentation) {
         basicRequest = given()
                 .baseUri("http://localhost")
-                .port(port);
+                .port(port)
+                .filter(documentationConfiguration(restDocumentation));
     }
 
     @Test
     @DisplayName("Spot 추가")
     void addSpot() {
-        Map<String, Object> requestBody = new HashMap<>(){{
+        Map<String, Object> requestBody = new HashMap<>() {{
             put("latitude", 200.0);
             put("longitude", 200.0);
         }};
         LocalDateTime timeBeforeRequest = LocalDateTime.now();
         ResponseBody body = given()
                 .spec(basicRequest).basePath("/api/spots")
+                .accept("application/json")
                 .contentType(ContentType.JSON).body(requestBody)
-                .when().post()
+                .filter(document("add-spot",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        requestFields(
+                                fieldWithPath("latitude").type(JsonFieldType.NUMBER).description("위도"),
+                                fieldWithPath("longitude").type(JsonFieldType.NUMBER).description("경도")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("latitude").type(JsonFieldType.NUMBER).description("위도"),
+                                fieldWithPath("longitude").type(JsonFieldType.NUMBER).description("경도"),
+                                fieldWithPath("createDt").type(JsonFieldType.STRING).description("기록된 날짜")
+                        )
+                )).when().post()
                 .then().statusCode(200)
                 .extract().response().body();
         LocalDateTime timeAfterRequest = LocalDateTime.now();
