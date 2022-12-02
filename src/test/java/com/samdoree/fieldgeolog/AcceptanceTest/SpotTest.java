069938ab404969f30e0a5573305fd97af73564ca
chisan01 <chisan2001@gmail.com>
2022-12-02 22:05:32 +0900

    test: Spot을 모두 읽어오는 기능 테스트 추가
    - rest docs 문서화 적용

--- src/test/java/com/samdoree/fieldgeolog/AcceptanceTest/SpotTest.java
+++ src/test/java/com/samdoree/fieldgeolog/AcceptanceTest/SpotTest.java
@@ -1,13 +1,14 @@
 package com.samdoree.fieldgeolog.AcceptanceTest;
 
 import com.samdoree.fieldgeolog.Spot.Entity.Spot;
+import com.samdoree.fieldgeolog.Spot.Repository.SpotRepository;
 import io.restassured.http.ContentType;
-import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -15,8 +16,8 @@ import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.samdoree.fieldgeolog.ApiDocumentUtils.getDocumentRequest;
 import static com.samdoree.fieldgeolog.ApiDocumentUtils.getDocumentResponse;
@@ -35,12 +36,26 @@ class SpotTest {
 
     private RequestSpecification basicRequest;
 
+    @Autowired
+    private SpotRepository spotRepository;
+
+    private final List<Spot> preGeneratedSpots = new ArrayList<>();
+
     @BeforeEach
     void setUp(RestDocumentationContextProvider restDocumentation) {
         basicRequest = given()
                 .baseUri("http://localhost")
                 .port(port)
                 .filter(documentationConfiguration(restDocumentation));
+
+        spotRepository.deleteAll();
+        for (int i = 0; i < 4; i++) {
+            preGeneratedSpots.add(spotRepository.save(Spot.builder()
+                    .latitude(100.0 + i)
+                    .longitude(100.0)
+                    .createDT(LocalDateTime.now())
+                    .build()));
+        }
     }
 
     @Test
@@ -81,7 +96,24 @@ class SpotTest {
     @Test
     @DisplayName("Spot 목록 불러오기")
     void getAllSpotList() {
-
+        Spot[] spots = given()
+                .spec(basicRequest).basePath("/api/spots")
+                .accept("application/json")
+                .contentType(ContentType.JSON)
+                .filter(document("read-all-spot",
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("id"),
+                                fieldWithPath("[].latitude").type(JsonFieldType.NUMBER).description("위도"),
+                                fieldWithPath("[].longitude").type(JsonFieldType.NUMBER).description("경도"),
+                                fieldWithPath("[].createDT").type(JsonFieldType.STRING).description("기록된 날짜")
+                        )
+                )).when().get()
+                .then().statusCode(200)
+                .log().all()
+                .extract().response().body().as(Spot[].class);
+        assertThat(preGeneratedSpots).hasSameElementsAs(Arrays.stream(spots).collect(Collectors.toList()));
     }
 
     @Test
