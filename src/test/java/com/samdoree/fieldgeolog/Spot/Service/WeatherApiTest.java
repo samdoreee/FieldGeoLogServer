4109d485d6f81fcc43968a70077ebb7ed8f38a88
chisan01 <chisan2001@gmail.com>
2022-12-06 20:15:55 +0900

    test: add failing test
    요청 시간에 따라 정상적으로 날씨를 못 받아오면서 에러가 발생함

--- src/test/java/com/samdoree/fieldgeolog/Spot/Service/WeatherApiTest.java
+++ src/test/java/com/samdoree/fieldgeolog/Spot/Service/WeatherApiTest.java
@@ -3,14 +3,21 @@ package com.samdoree.fieldgeolog.Spot.Service;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalTime;
 
 class WeatherApiTest {
 
     @Test
-    void restApiGetWeather() throws Exception {
-        WeatherApi weatherApi = new WeatherApi();
-        weatherApi.restApiGetWeather(LocalDateTime.now(), 12.3, 12.3);
+    void getWeatherInfo_at_now() throws Exception {
+        Double[] XY = WeatherApi.convertGRID_GPS(36.6287, 127.4606);
+        String weatherInfo = WeatherApi.getWeatherInfo(LocalDateTime.now(), XY[0], XY[1]);
+        System.out.println("weatherInfo = " + weatherInfo);
+    }
+
+    @Test
+    void getWeatherInfo_at_today_3AM() throws Exception {
+        Double[] XY = WeatherApi.convertGRID_GPS(36.6287, 127.4606);
+        String weatherInfo = WeatherApi.getWeatherInfo(LocalDateTime.of(LocalDateTime.now().toLocalDate(), LocalTime.of(3, 0)), XY[0], XY[1]);
+        System.out.println("weatherInfo = " + weatherInfo);
     }
 }
