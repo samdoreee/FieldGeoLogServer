e471af0550a0ca2adf904dbc4ba86c7aff496db6
JinYoungLee <brenda524@naver.com>
2023-10-01 23:22:02 +0900

    fix: Article Service에서 예외처리

--- src/main/java/com/samdoree/fieldgeolog/Comment/Repository/CommentRepository.java
+++ src/main/java/com/samdoree/fieldgeolog/Comment/Repository/CommentRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 @EnableJpaAuditing
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByArticleId(Long articleId);
+    Boolean existsByArticleId(Long articleId);
 }
