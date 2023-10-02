1ed8a038be80b0970e0c111a4b22c6af053f20a2
JinYoungLee <brenda524@naver.com>
2023-10-02 12:49:05 +0900

    feat: Repository에 @EnableJpaAuditing 추가하기

--- src/main/java/com/samdoree/fieldgeolog/Memo/Repository/MemoRepository.java
+++ src/main/java/com/samdoree/fieldgeolog/Memo/Repository/MemoRepository.java
@@ -2,9 +2,11 @@ package com.samdoree.fieldgeolog.Memo.Repository;
 
 import com.samdoree.fieldgeolog.Memo.Entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
+@EnableJpaAuditing
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findAllBySpotId(Long spotId);
 }
--- src/main/java/com/samdoree/fieldgeolog/Picture/Repository/PictureRepository.java
+++ src/main/java/com/samdoree/fieldgeolog/Picture/Repository/PictureRepository.java
@@ -2,9 +2,11 @@ package com.samdoree.fieldgeolog.Picture.Repository;
 
 import com.samdoree.fieldgeolog.Picture.Entity.Picture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.List;
 
+@EnableJpaAuditing
 public interface PictureRepository extends JpaRepository<Picture, Long> {
     List<Picture> findAllByMemoId(Long memoId);
     Boolean existsByMemoId(Long memoId);
